f6fc5f5c7ba65cc714dc657aa18ef005751e19c1
kwangsics <kwangsics@gmail.com>
2020-04-12 22:24:32 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import MyName from './MyName';
+import Counter from './Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        const style = {
+            backgroundColor: 'black',
+            padding: '16px',
+            color: 'white',
+            fontSize: '12px'
+        };
+
+        return (
+            <div>
+                <div style={style}>
+                    hi there
+                </div>
+                <div className="reactClass">
+                    dskjfjksafjlksa
+                </div>
+                <MyName name = "rrrr" />
+
+                <Counter />
+            </div>
+        );
+    }
 }
 
 export default App;
